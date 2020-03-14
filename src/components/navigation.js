8b76ad9a6faa17ac44342056c28be4bd874efc46
faizhameed <faizftb@gmail.com>
2020-03-14 19:23:16 +0530

    Adjust navigation

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import styles from './navigation.module.css'
+import Logo from '../../static/logo.jpg'
 
 export default () => (
   <nav role="navigation">
+    <div className={styles.logoContainer}>
+      <img className={styles.logo} src={Logo} />
+      <h3>NEWS FIRST</h3>
+      <p>Be a part of it</p>
+    </div>
     <ul className={styles.navigation}>
       <li className={styles.navigationItem}>
         <Link
