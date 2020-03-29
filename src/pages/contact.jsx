e22696491779114982970bb947ec6d7fdb10562e
faizhameed <faizftb@gmail.com>
2020-03-29 21:40:02 +0530

    adding contact page form

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,9 +4,26 @@ import Styles from './contact.module.css'
 const ContactUs = () => {
   return (
     <Layout>
-      <div className={Styles.hero}>
-        <h1>Welcome to Contact us page</h1>
-      </div>
+      <form method="post" action="#">
+        <label>
+          Name
+          <input type="text" name="name" id="name" />
+        </label>
+        <label>
+          Email
+          <input type="email" name="email" id="email" />
+        </label>
+        <label>
+          Subject
+          <input type="text" name="subject" id="subject" />
+        </label>
+        <label>
+          Message
+          <textarea name="message" id="message" rows="5" />
+        </label>
+        <button type="submit">Send</button>
+        <input type="reset" value="Clear" />
+      </form>
     </Layout>
   )
 }
