834024bc7a77463148952f55a9613d59ebb456c7
Faiz Hameed <faizftb@gmail.com>
2020-04-22 22:23:14 +0530

    contact us page rebuild

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,29 +1,30 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Styles from './contact.module.css'
+import Styles from './contact.module.scss'
 const ContactUs = () => {
   return (
     <Layout>
-      <form method="post" action="#">
-        <label>
-          Name
-          <input type="text" name="name" id="name" />
-        </label>
-        <label>
-          Email
-          <input type="email" name="email" id="email" />
-        </label>
-        <label>
-          Subject
-          <input type="text" name="subject" id="subject" />
-        </label>
-        <label>
-          Message
-          <textarea name="message" id="message" rows="5" />
-        </label>
-        <button type="submit">Send</button>
-        <input type="reset" value="Clear" />
-      </form>
+      <div className={Styles.contactPageContainer}>
+        <form method="post" action="#">
+          <label>
+            <p> Name</p>
+            <input type="text" name="name" id="name" />
+          </label>
+          <label>
+            <p> Email</p>
+            <input type="email" name="email" id="email" />
+          </label>
+          <label>
+            <p>Subject</p>
+            <input type="text" name="subject" id="subject" />
+          </label>
+          <label>
+            <p> Message</p>
+            <textarea name="message" id="message" rows="5" />
+          </label>
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </Layout>
   )
 }
